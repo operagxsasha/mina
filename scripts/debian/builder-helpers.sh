f087a44c29d11812cbbf785aa786e2e4e89a49ca
dkijania <dariusz@o1labs.org>
2024-11-17 21:18:37 +0100

    adjust archive test folder

--- scripts/debian/builder-helpers.sh
+++ scripts/debian/builder-helpers.sh
@@ -247,7 +247,7 @@ build_functional_test_suite_deb() {
   cp ./default/src/test/archive/archive_node_tests/archive_node_tests.exe "${BUILDDIR}/usr/local/bin/mina-archive-node-test"
 
   mkdir -p ${BUILDDIR}/etc/mina/test/archive/sample_db
-  rsync -Huav ../src/test/archive/sample_db* "${BUILDDIR}/etc/mina/test/archive/sample_db"
+  rsync -Huav ../src/test/archive/sample_db* "${BUILDDIR}/etc/mina/test/archive"
 
   build_deb mina-test-suite
 

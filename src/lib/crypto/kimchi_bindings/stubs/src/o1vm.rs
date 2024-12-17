683705bc9da3ea2128553d608234b82418d0b8ee
martyall <martyall@protonmail.com>
2024-12-17 12:14:47 -0800

    caml_o1vm_proof_create

--- src/lib/crypto/kimchi_bindings/stubs/src/o1vm.rs
+++ src/lib/crypto/kimchi_bindings/stubs/src/o1vm.rs
@@ -1,65 +1,41 @@
-use crate::{
-    arkworks::{CamlFp, CamlGVesta},
-    field_vector::fp::CamlFpVector,
-    pasta_fp_plonk_index::{CamlPastaFpPlonkIndex, CamlPastaFpPlonkIndexPtr},
-    pasta_fp_plonk_verifier_index::CamlPastaFpPlonkVerifierIndex,
-    srs::fp::CamlFpSrs,
-    WithLagrangeBasis,
-};
-use ark_ec::AffineRepr;
-use ark_ff::One;
-use array_init::array_init;
-use groupmap::GroupMap;
-use kimchi::verifier::verify;
-use kimchi::{
-    circuits::lookup::runtime_tables::{caml::CamlRuntimeTable, RuntimeTable},
-    prover_index::ProverIndex,
-};
-use kimchi::{circuits::polynomial::COLUMNS, verifier::batch_verify};
-use kimchi::{
-    proof::{
-        PointEvaluations, ProofEvaluations, ProverCommitments, ProverProof, RecursionChallenge,
-    },
-    verifier::Context,
-};
-use kimchi::{prover::caml::CamlProofWithPublic, verifier_index::VerifierIndex};
-use mina_curves::pasta::{Fp, Fq, Pallas, Vesta, VestaParameters};
+use crate::arkworks::{CamlFp, CamlGVesta};
+use kimchi::circuits::domains::EvaluationDomains;
+use kimchi_msm::expr::E;
+use mina_curves::pasta::{Fp, Vesta, VestaParameters};
 use mina_poseidon::{
     constants::PlonkSpongeConstantsKimchi,
     sponge::{DefaultFqSponge, DefaultFrSponge},
 };
-use o1vm::prover;
-use poly_commitment::commitment::{CommitmentCurve, PolyComm};
-use poly_commitment::ipa::OpeningProof;
-use std::array;
-use std::convert::TryInto;
+use o1vm::{
+    interpreters::mips::constraints as mips_constraints,
+    pickles::{proof, prover},
+};
+use poly_commitment::{ipa::SRS, SRS as _};
 
-type EFqSponge = DefaultFqSponge<VestaParameters, PlonkSpongeConstantsKimchi>;
-type EFrSponge = DefaultFrSponge<Fp, PlonkSpongeConstantsKimchi>;
+pub const DOMAIN_SIZE: usize = 1 << 15;
 
 #[ocaml_gen::func]
 #[ocaml::func]
 pub fn caml_o1vm_proof_create(
-    index: CamlPastaFpPlonkIndexPtr<'static>,
-    witness: Vec<CamlFpVector>,
-    runtime_tables: Vec<CamlRuntimeTable<CamlFp>>,
-    prev_challenges: Vec<CamlFp>,
-    prev_sgs: Vec<CamlGVesta>,
-) -> Result<CamlProofWithPublic<CamlGVesta, CamlFp>, ocaml::Error> {
-    #[ocaml_gen::func]
-    #[ocaml::func]
-    pub fn caml_o1vm_proof_create(
-        domain: CamlEvaluationDomains<CamlFp>,
-        srs: CamlFpSrs,
-        inputs: CamlProofInputs<CamlFp>,
-        constraints: &[E<CamlFp>],
-    ) -> Result<CamlProof<CamlFp>, ProverError>
+    inputs: proof::caml::CamlProofInputs<CamlFp>,
+) -> Result<proof::caml::CamlProof<CamlGVesta, CamlFp>, prover::ProverError>
 where {
-        let domain = domain.into();
-        let srs = srs.into();
-        let inputs = inputs.into();
-        let constraints = constraints.into();
-        let proof = prover::create_proof(domain, srs, inputs, constraints)?;
-        Ok(proof.into())
-    }
+    // TODO use static values
+    let constraints: Vec<E<Fp>> = mips_constraints::make_constraints::<Fp>();
+    let domain_fp: EvaluationDomains<Fp> = EvaluationDomains::create(DOMAIN_SIZE).unwrap();
+    let srs: SRS<Vesta> = {
+        let srs = SRS::create(DOMAIN_SIZE);
+        srs.get_lagrange_basis(domain_fp.d1);
+        srs
+    };
+
+    let inputs = inputs.into();
+    let mut rng = rand::thread_rng();
+    let proof = prover::prove::<
+        Vesta,
+        DefaultFqSponge<VestaParameters, PlonkSpongeConstantsKimchi>,
+        DefaultFrSponge<Fp, PlonkSpongeConstantsKimchi>,
+        _,
+    >(domain_fp, &srs, inputs, &constraints, &mut rng)?;
+    Ok(proof.into())
 }
